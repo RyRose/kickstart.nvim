20f3807aba8aad1296c44b4fb564316d9d8896dc
RyRose <RyRose@users.noreply.github.com>
2024-09-08 02:07:29 -0500

    Enable pyright.

--- init.lua
+++ init.lua
@@ -613,7 +613,7 @@ require('lazy').setup({
         rust_analyzer = {},
         gopls = {},
         -- bashls = {},
-        -- pyright = {},
+        pyright = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
